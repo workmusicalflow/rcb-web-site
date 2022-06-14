5f02a90a77ca901498725a44f180abb09c96b907
workmusicalflow <studioabidjanpro1@gmail.com>
2022-06-14 19:41:56 +0000

    Premier commit RCB

---
+++ app.js
@@ -0,0 +1,15 @@
+const btnMenu = document.querySelector('logo-menu');
+const menu = document.querySelector('liste-nav');
+
+btnMenu.addEventListener('click', () => {
+    menu.classList.toggle('active')
+})
+
+const allLinks = document.querySelector('.item-nav');
+
+allLinks.forEach(item => {
+    
+    item.addEventListener('click', {} => {
+        menu.classList.toggle('active')
+    })
+})
